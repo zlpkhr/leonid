38bbe6bf49e22e27fd2063633803d0446881035b
Azamat Zulpykhar <zlpkhr@icloud.com>
2024-12-02 02:50:29 +0100

    Cleanup

--- src/lenet.py
+++ src/lenet.py
@@ -1,6 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torch.utils.data as data
+from torch.utils.data import DataLoader
+import torch.optim as optim
 
 
 class Lenet(nn.Module):
@@ -28,3 +33,79 @@ class Lenet(nn.Module):
         x = F.relu(x)
         x = self.f7(x)
         return x
+
+
+def train_model(model, train_loader, validation_loader, epochs=3):
+    optimizer = optim.Adam(model.parameters())
+
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+
+        for i, (inputs, labels) in enumerate(train_loader, 1):
+            inputs, labels = inputs, labels
+
+            optimizer.zero_grad()
+
+            activation = model(inputs)
+            loss = F.cross_entropy(activation, labels)
+
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 100 == 0:
+                print(f"Epoch {epoch + 1}, Batch {i}, Loss: {running_loss/100:.3f}")
+                running_loss = 0.0
+
+        model.eval()
+        correct = total = 0
+
+        with torch.no_grad():
+            for inputs, labels in validation_loader:
+                _, predicted = model(inputs).max(1)
+                total += labels.size(0)
+                correct += predicted.eq(labels).sum().item()
+
+        print(f"Epoch {epoch + 1}, Validation Accuracy: {100. * correct / total:.2f}%")
+
+
+if __name__ == "__main__":
+    transform = transforms.Compose(
+        [
+            transforms.Pad(2),
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,)),
+        ]
+    )
+
+    train_set = datasets.MNIST(
+        root="tmp/datasets", train=True, download=True, transform=transform
+    )
+    test_set = datasets.MNIST(
+        root="tmp/datasets", train=False, download=True, transform=transform
+    )
+
+    train_set_size = int(len(train_set) * 0.8)
+    val_set_size = len(train_set) - train_set_size
+
+    train_set, val_set = data.random_split(train_set, [train_set_size, val_set_size])
+
+    train_loader = DataLoader(train_set, shuffle=True)
+    val_loader = DataLoader(val_set)
+    test_loader = DataLoader(test_set)
+
+    model = Lenet()
+    train_model(model, train_loader, val_loader)
+
+    model.eval()
+    correct = total = 0
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            inputs, labels = inputs, labels
+            _, predicted = model(inputs).max(1)
+            total += labels.size(0)
+            correct += predicted.eq(labels).sum().item()
+    print(f"Test Accuracy: {100. * correct / total:.2f}%")
+
+    torch.jit.script(model).save("tmp/lenet.pt")
